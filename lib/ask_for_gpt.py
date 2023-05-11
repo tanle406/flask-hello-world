2b55ffd59a924cf086cd5f0dcbefa8f9e7687a05
“VNEXT” <tanlv@vnext.vn>
2023-05-11 16:14:16 +0700

    remove pywin32

--- lib/ask_for_gpt.py
+++ lib/ask_for_gpt.py
@@ -5,15 +5,18 @@ model_engine = "gpt-3.5-turbo"
 
 def ask_chatgpt(question):
     print("1")
-    response = openai.ChatCompletion.create(
-        model='gpt-3.5-turbo',
-        n=1,
-        messages=[
-            {"role": "system", "content": \
-                "You are a helpful assistant with exciting, \
-                interesting things to say."},
-            {"role": "user", "content": question},
-        ])
-    print("2")
-    message = response.choices[0]['message']
-    return message['content']
+    try:
+        response = openai.ChatCompletion.create(
+            model='gpt-3.5-turbo',
+            n=1,
+            messages=[
+                {"role": "system", "content": \
+                    "You are a helpful assistant with exciting, \
+                    interesting things to say."},
+                {"role": "user", "content": question},
+            ])
+        print("2")
+        message = response.choices[0]['message']
+        return message['content']
+    except Exception as e:
+        print("ERROR: " + str(e))
